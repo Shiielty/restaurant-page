1abd6fcf86ef5e35967833fdef407827638f161f
Shiielty <kazutonine@gmail.com>
2022-06-06 09:28:13 +0700

    set up npm init :: set up webpack :: set up index.html and index.js then generate main.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    devtool: "inline-source-map",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
