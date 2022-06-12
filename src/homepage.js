410f5dc2499e2a92644e5cf9c8c13d4c705538fa
Shiielty <kazutonine@gmail.com>
2022-06-12 23:21:22 +0700

    test(*): create navbar in homepage module result: I have a hard time to styling it and too much work to make it in js the first time, I should make it directly in HTML and CSS first

--- src/homepage.js
+++ src/homepage.js
@@ -1,8 +1,22 @@
-const homePage = () => {
-    const test = document.createElement("div");
-    test.textContent = "Hello, world!!!";
-    return test;
-}
+const homePage = () => console.log("Homepage Loaded!");
+
+// const navArr = ["HOME", "MENU", "ABOUT US", "BOOKING"]
+// const nav = () => {
+//     const navbar = document.createElement("div");
+//     navbar.classList.add("navbar");
+
+//     const navList = document.createElement("ul");
+//     navArr.forEach((menu) => {
+//         const navMenu = document.createElement("li");
+//         navMenu.classList.add("nav-menu");
+//         navMenu.textContent = menu;
+//         navList.appendChild(navMenu);
+//     })
+
+//     navbar.appendChild(navList);
+
+//     return navbar;
+// }
 
 
 export { homePage }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
-import {homePage} from './homepage.js'
+import { homePage } from './homepage.js'
 
 console.log("Hello, world!");
 console.log("Loading css...")
-const testDOM = homePage();
-const content = document.querySelector("#content")
-content.appendChild(testDOM);
+console.log(homePage());
