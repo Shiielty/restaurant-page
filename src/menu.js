17266b372c7ebc3f053e0d077bb8d0570fbb3be3
Shiielty <kazutonine@gmail.com>
2022-09-02 11:01:05 +0700

    docs(*): write the logic to create the menu page

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,12 @@
 const menuPage = () => console.log("Loading menu... Success!!");
 
+// declare menu names and price in object, dumpling and noodle soup
+// Menu page structure: header menu (h1) + (menu container > menu heading (h2) + menu names (p)*n)*2
+// create dom elements: 
+//  - header menu (h1) * 1
+//  - menu container (div) * 2
+//  - menu heading (h2) * 1 each container
+//  - menu names (p) * n each container --> loop to input all menu names and prices
+// append all of that to menuContent and export it as menu *page*
+
 export {menuPage}
