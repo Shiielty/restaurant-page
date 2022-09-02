59bff4282cb07b3ff4238f5ecc6ab84e0ad5228d
Shiielty <kazutonine@gmail.com>
2022-09-02 15:21:52 +0700

    feat(*): create 'createMenu' function to generate menu's DOM and refactor the codes

--- src/menu.js
+++ src/menu.js
@@ -13,7 +13,7 @@ const noodles = {
     "Chiken Noodle Soup": 5.75,
     "House Special Noodle Soup": 5.75,
     "Shrimp Lo Mein": 5.75,
-    "Vegetable Lo Mein": 5.00
+    "Vegetable Lo Mein": 5.00,
 }
 
 const test = () => {
@@ -22,53 +22,60 @@ const test = () => {
     }
 }
 
-// Menu page structure: header menu (h1) + (menu container > menu heading (h2) + menu names (p)*n)*2
+const createMenu = (menuType, type, obj, name) => {
+    const menuContainer = document.createElement("div");
+    const menuName = document.createElement("p");
+    const divider = document.createElement("div");
+    const menuPrice = document.createElement("p");
+    
+    menuContainer.classList.add("menu-container");
+    menuName.classList.add("menu-name");
+    divider.classList.add("divider");
+    menuPrice.classList.add("menu-price");
+    
+    menuType.textContent = type;
+    menuName.textContent = `${name}`;
+    menuPrice.textContent = `$${obj[name].toFixed(2)}`;
+    
+    menuContainer.appendChild(menuName);
+    menuContainer.appendChild(divider);
+    menuContainer.appendChild(menuPrice);
 
+    return menuContainer;
+}
+
+// Menu page structure: header menu (h1) + (menu container > menu heading (h2) + menu names (p)*n)*2
 const menu = () => {
     
-    // Create dom
     const container = document.createElement("div");
     const menuHead = document.createElement("h1");
     
+    container.classList.add("menu-content");
+    menuHead.classList.add("menu-head");
+
     menuHead.textContent = "Menu";
 
     container.appendChild(menuHead);
 
     for (let i = 0; i < 2; i++) {
-        const menusContainer = document.createElement("div");
         const menuType = document.createElement("h3");
+        const menusContainer = document.createElement("div");
+
+        menuType.classList.add("menu-type");
 
         menusContainer.appendChild(menuType);
 
         if (i === 0){
             for (const name in dumplings) {
-                const menuContainer = document.createElement("div");
-                const menuName = document.createElement("p");
-                const menuPrice = document.createElement("p");
-                
-                menuName.textContent = `${name}`;
-                menuPrice.textContent = `$${dumplings[name]}`;
-                menuType.textContent = "Dumplings"
-                
-                menuContainer.appendChild(menuName);
-                menuContainer.appendChild(menuPrice);
-                menusContainer.appendChild(menuContainer);
+                const dumplingMenus = createMenu(menuType, "Dumplings", dumplings, name);
+                menusContainer.appendChild(dumplingMenus);
                 container.appendChild(menusContainer);
             }
         }
         else {
             for (const name in noodles) {
-                const menuContainer = document.createElement("div");
-                const menuName = document.createElement("p");
-                const menuPrice = document.createElement("p");
-                
-                menuType.textContent = "Noodles"
-                menuName.textContent = `${name}`;
-                menuPrice.textContent = `$${noodles[name]}`;
-                
-                menuContainer.appendChild(menuName);
-                menuContainer.appendChild(menuPrice);
-                menusContainer.appendChild(menuContainer);
+                const noodleMenus = createMenu(menuType,"Noodles", noodles, name);
+                menusContainer.appendChild(noodleMenus);
                 container.appendChild(menusContainer);
             }
         }
