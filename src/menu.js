c71dedabcd14e96dc26a73129c35f4ddabf23d83
Shiielty <kazutonine@gmail.com>
2022-09-02 11:29:34 +0700

    feat(*): add dumpling and noodle object

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,21 @@
 const menuPage = () => console.log("Loading menu... Success!!");
 
 // declare menu names and price in object, dumpling and noodle soup
+
+const dumplings = {
+    "Chiken Dumpling": 6.25,
+    "Beef Dumpling": 6.25,
+    "Vegetable Dumpling": 6.00,
+    "Peking Duck Spring Roll": 6.00, 
+}
+const noodles = {
+    "Spicy Beef Stew Noodle Soup": 5.75,
+    "Chiken Noodle Soup": 5.75,
+    "House Special Noodle Soup": 5.75,
+    "Shrimp Lo Mein": 5.75,
+    "Vegetable Lo Mein": 5.00
+}
+
 // Menu page structure: header menu (h1) + (menu container > menu heading (h2) + menu names (p)*n)*2
 // create dom elements: 
 //  - header menu (h1) * 1
