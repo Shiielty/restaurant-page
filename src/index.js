095ac2b1791fa30d63754e924e8e58e96d190bdb
Shiielty <kazutonine@gmail.com>
2022-09-05 19:01:18 +0700

    refactor(index.js): use event delegation for the event listener, now just one event listener exist

--- src/index.js
+++ src/index.js
@@ -1,61 +1,65 @@
-import { homePage, nav, mainContent } from "./homepage.js";
-import { menuPage, menu } from "./menu.js";
-import { aboutUsPage, about } from "./aboutUs.js";
-import { bookingPage, booking } from "./booking.js";
+import { nav, mainContent } from "./homepage.js";
+import { menu } from "./menu.js";
+import { about } from "./aboutUs.js";
+import { booking } from "./booking.js";
 import "./style.css";
 
-console.log(homePage());
-console.log(menuPage());
-console.log(aboutUsPage());
-console.log(bookingPage());
+const components = (() => {
+  const navbar = nav();
+  const homePage = mainContent();
+  const menuPage = menu();
+  const aboutPage = about();
+  const bookingPage = booking();
 
-const navComponent = nav();
-const mainContentComponent = mainContent();
-const menuComponent = menu();
-const aboutComponent = about();
-const bookingComponent = booking();
+  return { navbar, homePage, menuPage, aboutPage, bookingPage };
+})();
+
+/* remove the second child */
+const clear = (parent) => {
+  parent.removeChild(parent.childNodes[1]);
+}
+
+const navColor = (oppacity) => {
+  navbar.style.backgroundColor = `rgb(0, 0, 0, ${oppacity})`;
+}
+
+const changeTab = (e) => {
+  const targetClass = e.target.className;
+  const targetId = e.target.id
+  switch (true) {
+    case targetClass.includes("home-btn"):
+      clear(content);
+      content.appendChild(components.homePage);
+      navColor(0.3);
+      break;
+    case targetClass.includes("menu-btn"):
+      clear(content);
+      content.appendChild(components.menuPage);
+      navColor(1);
+      break;
+    case targetClass.includes("about-btn"):
+      clear(content);
+      content.appendChild(components.aboutPage);
+      navColor(1);
+      break;
+    case targetId.includes("booking"):
+      clear(content);
+      content.appendChild(components.bookingPage);
+      navColor(1);
+      break;
+    default:
+      break;
+  }
+}
 
 const content = document.querySelector("#content");
-content.appendChild(navComponent);
-content.appendChild(mainContentComponent);
+content.appendChild(components.navbar);
+content.appendChild(components.homePage);
 
+const navbar = document.querySelector(".navbar");
 const homeBtn = document.querySelectorAll(".home-btn");
 const menuBtn = document.querySelectorAll(".menu-btn");
 const aboutBtn = document.querySelector(".about-btn")
 const bookingBtn = document.querySelector("#booking")
 
-homeBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    content.removeChild(content.childNodes[1]);
-    content.appendChild(mainContentComponent);
-
-    const nav = document.querySelector(".navbar");
-    nav.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
-  });
-});
-
-menuBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    content.removeChild(content.childNodes[1]);
-    content.appendChild(menuComponent);
-
-    const nav = document.querySelector(".navbar");
-    nav.style.backgroundColor = "rgb(0, 0, 0, 1)";
-  });
-});
-
-aboutBtn.addEventListener("click", () => {
-  content.removeChild(content.childNodes[1]);
-  content.appendChild(aboutComponent);
-
-  const nav = document.querySelector(".navbar");
-  nav.style.backgroundColor = "rgb(0, 0, 0, 1)";
-})
-
-bookingBtn.addEventListener("click", () => {
-  content.removeChild(content.childNodes[1]);
-  content.appendChild(bookingComponent);
-
-  const nav = document.querySelector(".navbar");
-  nav.style.backgroundColor = "rgb(0, 0, 0, 1)";
-})
+content.addEventListener("click", changeTab);
